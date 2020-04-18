d3426243e1d72d1bbb2e7afb344c70fcd425dcbb
Marcus Lozano <contato@lozanno.com.br>
2020-04-18 00:45:51 -0300

    Create Posts and Photos components

---
+++ src/components/Photos/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Photos = () => <h1>Photos</h1>;
+
+export default Photos;
---
+++ src/components/Posts/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+// import { Container } from './styles';
+
+const Posts = () => {
+  return (
+    <>
+      <h1>Posts</h1>
+    </>
+  );
+};
+
+export default Posts;
