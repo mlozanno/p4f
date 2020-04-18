6eec56ec70bd76c9638c120b2b2ad9fd9d6ab9c2
Marcus Lozano <contato@lozanno.com.br>
2020-04-18 03:25:41 -0300

    Create PostItem component

---
+++ src/components/PostItem/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { StyledPostItem } from './styles';
+
+const PostItem = ({
+  title = 'Post sem título',
+  body = 'Post sem descrição',
+}) => (
+  <StyledPostItem>
+    <header>
+      <h1>{title}</h1>
+    </header>
+    <p>{body}</p>
+  </StyledPostItem>
+);
+
+PostItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+};
+
+export default PostItem;
