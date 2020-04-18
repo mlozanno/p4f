438674ea5f55f2540d503faf83b2c896c7cfdc5d
Marcus Lozano <contato@lozanno.com.br>
2020-04-18 03:28:40 -0300

    Remove box style

--- src/components/Content/styles.js
+++ src/components/Content/styles.js
@@ -4,9 +4,5 @@ export const StyledContent = styled.section`
   margin: ${({ theme }) => theme.metric(2, 'auto', 2)};
   padding: ${({ theme }) => theme.metric(0, 2)};
   color: #333;
-  border-radius: 0.6rem;
-  padding: ${({ theme }) => theme.metric(2)};
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.16);
   width: calc(100% - ${({ theme }) => theme.metric(4)});
-  background-color: #fff;
 `;
