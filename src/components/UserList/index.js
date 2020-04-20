f020a6fdbfbed6eec94312ed860f31ad05b17a32
Marcus Lozano <contato@lozanno.com.br>
2020-04-19 23:09:57 -0300

    Revert "Refactor and abstraction UserList/Home component"
    This reverts commit 8776e64aefb918f108cb13df34b659c43eecb144.

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,49 +1,12 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-
 import PropTypes from 'prop-types';
 
 import { StyledUserList } from './styles';
 
-import UserCard from '../UserCard';
-
-const UserList = ({ users, pathName }) => {
-	const history = useHistory();
-
-	const handleClick = (id) => {
-		history.push(`/${id}/${pathName}`);
-	};
-	return (
-		<StyledUserList>
-			{users.map((user) => (
-				<UserCard
-					key={user.id}
-					data-user-id={user.id}
-					name={user.name}
-					posts={user.posts}
-					email={user.email}
-					company={user.company.name}
-					handleClick={() => handleClick(user.id)}
-				/>
-			))}
-		</StyledUserList>
-	);
-};
+const UserList = ({ children }) => <StyledUserList>{children}</StyledUserList>;
 
 UserList.propTypes = {
-	users: PropTypes.arrayOf(
-		PropTypes.shape({
-			name: PropTypes.string,
-			url: PropTypes.string,
-			title: PropTypes.string,
-		})
-	),
-	pathName: PropTypes.string,
-};
-
-UserList.defaultProps = {
-	users: [],
-	pathName: 'photos',
+	children: PropTypes.node.isRequired,
 };
 
 export default UserList;
